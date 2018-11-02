7b4e8210e4a507b2e8962b1f42175d5e9e06110a
iaycan96 <ibrahimaycan@marun.edu.tr>
2018-11-02 20:45:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StartGame sg = new StartGame();
+        sg.start();
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,11 @@
+
+public class Player {
+    public String name;
+    private int currentplace;
+    public int getCurrentplace() {
+        return currentplace;
+    }
+    public void setCurrentplace(int currentplace) {
+        this.currentplace = currentplace;
+    }
+}
