273d70c02f537007ba6ae5b92e6c874ced5f027c
iaycan96 <ibrahimaycan@marun.edu.tr>
2018-11-23 11:10:43 +0300

    upload code

--- src/Square.java
+++ src/Square.java
@@ -1,5 +1,5 @@
-abstract class Square {
-    private String name="";
+public abstract class Square {
+    private String name;
     private int location=0;
 
 
@@ -12,12 +12,16 @@ abstract class Square {
     }
 
     public void setlocation(int location ){
+
         this.location =location;
 
     }
     public int getLocation(){
+
         return this.location;
     }
+
+    public abstract void action(Player p, int Die1,int Die2) ;
 
 
 }
