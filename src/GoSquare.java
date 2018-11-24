0a2cf629eee68a77c4aa16ee34927221c3714a5d
sabriye köymen <sabriyekoymen@hotmail.com.tr>
2018-11-24 23:33:01 +0300

    Square's GoSquare and JailSquare classes are added.

---
+++ src/GoSquare.java
@@ -0,0 +1,11 @@
+class GoSquare extends Square {
+    public GoSquare(int index) {
+
+        super.setname("Go Square");
+        super.setlocation(index);
+    }
+    @Override
+    public void action(Player p,int d1,int d2){
+
+    }
+}
